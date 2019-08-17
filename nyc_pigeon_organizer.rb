f833f3d87da51120e5abb651d9cebbe2a1ee4ec5
Krista <krista.loven.87@gmail.com>
2019-08-17 19:50:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,9 @@ def nyc_pigeon_organizer(data)
     specs.each do |quality, names|
       names.each do |name|
         if pigeons[name]
-          pigeons[name][category] ? pigeons[name][category] << quality : pigeons[name][category] = [quality]
+          pigeons[name][category] ? pigeons[name][category] << quality.to_s : pigeons[name][category] = [quality]
         else
-          pigeons[name] = {category => [quality]}
+          pigeons[name] = {category => [quality.to_s]}
         end
       end
     end
