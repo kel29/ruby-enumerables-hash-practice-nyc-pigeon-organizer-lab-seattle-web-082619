df3acb974ed4139780197d67275925955b5edb59
Krista <krista.loven.87@gmail.com>
2019-08-17 19:18:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,6 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # pull pidgeon names from gender 
+  puts data[:gender].each
+  # add each pidgeon as a key in a new has 
+  # iterate through the rest of the items to get the info into the new hash
 end
