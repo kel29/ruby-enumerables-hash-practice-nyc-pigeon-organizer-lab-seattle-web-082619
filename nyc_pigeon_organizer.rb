6da9d631768cce264f7f7b07365114b848ea3432
Krista <krista.loven.87@gmail.com>
2019-08-17 19:39:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-
+  pigeons = {}
+  data.each do |category, specs|
+    specs.each do |quality, names|
+      names.each do |name|
+        if pigeons[name]
+          puts "#{name} is already in the hash"
+        else
+          pigeons[name] = {category => quality}
+        end
+      end
+    end
+  end
+  pigeons
 end
